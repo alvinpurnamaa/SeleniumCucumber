9c0a815f138eb8d21edce9b212a20505fb667c06
alvinpurnamaa <alvinpurnama26@gmail.com>
2026-04-27 00:03:41 +0700

    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/alvin/APITest.java
+++ src/test/java/com/alvin/APITest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class APITest {
 
@@ -12,11 +13,16 @@ public class APITest {
     public void getUsersTest() {
         RestAssured.baseURI = "https://gorest.co.in/public/v2";
 
-        given()
-                .when()
-                .get("/users")
-                .then()
-                .log().all()
-                .statusCode(200);
+        int statusCode =
+                given()
+                        .when()
+                        .get("/users")
+                        .then()
+                        .log().all()
+                        .extract()
+                        .statusCode();
+
+        // Accept 200 or 204 (kadang API kosong)
+        assertTrue(statusCode == 200 || statusCode == 204);
     }
 }
--- src/test/java/com/alvin/stepdef/ApiStepDef.java
+++ src/test/java/com/alvin/stepdef/ApiStepDef.java
@@ -3,17 +3,23 @@ package com.alvin.stepdef;
 import io.cucumber.java.en.Given;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApiStepDef {
 
     @Given("user get gorest users")
     public void getGorestUsers() {
-        given()
-                .baseUri("https://gorest.co.in/public/v2")
-                .when()
-                .get("/users")
-                .then()
-                .log().all()
-                .statusCode(200);
+
+        int statusCode =
+                given()
+                        .baseUri("https://gorest.co.in/public/v2")
+                        .when()
+                        .get("/users")
+                        .then()
+                        .log().all()
+                        .extract()
+                        .statusCode();
+
+        assertTrue(statusCode == 200 || statusCode == 204);
     }
 }
